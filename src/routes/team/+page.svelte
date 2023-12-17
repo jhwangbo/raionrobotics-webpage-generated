a73d3798378b9293b3bce8d42e172a4ed3c0bbc0
jhwangbo <jemin.hwangbo2@gmail.com>
2023-12-17 15:03:52 +0900

    removed jemin picture

--- src/routes/team/+page.svelte
+++ src/routes/team/+page.svelte
@@ -8,6 +8,13 @@
 
 <section id="hero" style="width: 600px;margin-left: auto;margin-right: auto;" class="font-noto bg-gray-100">
 
+	<div style="margin: 10px;">
+		<p>(주)라이온로보틱스는 카이스트 기계공학과 라이랩의 스핀오프 기업입니다.
+		</p>
+		<p>본사의 연구 개발은 황보제민 교수의 연구실 출신 학생들이 주축을 이루고 있습니다. 이미 3명의 학생이 합류하였고 더 많은 학생들이 졸업 후 합류 할 예정입니다.
+			또한, 황보제민 교수의 연구실에서 생산되는 보행 로봇 원천기술도 흡수하여 미래에도 세계 최고의 기술력을 유지할 것입니다. 	
+		</p>
+	</div>
 </section>
 
 <style>
